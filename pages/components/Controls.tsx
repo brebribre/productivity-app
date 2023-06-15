7bbd1f6bb330646f03968392405ad12cacf861d7
brebribre <70563428+brebribre@users.noreply.github.com>
2023-06-15 21:48:59 +0200

    final timer pls

--- pages/components/Controls.tsx
+++ pages/components/Controls.tsx
@@ -5,16 +5,19 @@ import Image from 'next/image'
 export default function Controls(props:any){
  
     return (
-    <div className = " mt-4 py-3 bg-white grid grid-cols-2 gap-4 border-4 rounded-xl text-slate-500 font-bold">
-        <div 
-            className="my-auto mx-auto transform transition duration-200 hover:opacity-80 hover:scale-110" 
-            onClick = {props.onPlay}
-        >
-            {props.isActive? <Image src={'pause.svg'} alt="pause-button" width={30} height={30}/> : <Image src={'play.svg'} alt="play-button" width={30} height={30}/>}
-        </div>
 
-        <div className="my-auto mx-auto transform transition duration-200 hover:opacity-80 hover:scale-110" onClick = {props.onReset}><Image src={'reset.svg'} alt="reset-button" width={30} height={30}/></div>
-    </div>
+            <div className = "w-80 mx-auto mt-4 py-3 bg-white grid grid-cols-2 gap-4 border-4 rounded-xl text-slate-500 font-bold">
+                <div 
+                    className="my-auto mx-auto transform transition duration-200 hover:opacity-80 hover:scale-110" 
+                    onClick = {props.onPlay}
+                >
+                    {props.isActive? <Image src={'pause.svg'} alt="pause-button" width={30} height={30}/> : <Image src={'play.svg'} alt="play-button" width={30} height={30}/>}
+                </div>
+
+                <div className="my-auto mx-auto transform transition duration-200 hover:opacity-80 hover:scale-110" onClick = {props.onReset}><Image src={'reset.svg'} alt="reset-button" width={30} height={30}/></div>
+            </div>
+     
+    
     )
 
 }
