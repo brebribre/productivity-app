55cda7b344b88964ac23fc37fa768249ca15e416
brebribre <70563428+brebribre@users.noreply.github.com>
2023-06-18 18:25:41 +0200

    testig

--- pages/components/Todo.tsx
+++ pages/components/Todo.tsx
@@ -11,7 +11,7 @@ export default function Todo(props:any){
     }
 
     return (
-        <div className = "flex border-b-2 mt-6">
+        <div className = "flex border-b-2 mt-6 my-auto">
             <input 
                 type="checkbox" 
                 className="flex-auto m-2 rounded-xl transform transition duration-200 hover:opacity-80 hover:scale-125" 
@@ -20,13 +20,13 @@ export default function Todo(props:any){
             />
 
             <p 
-                className = {`flex-auto w-48 my-auto px-2 text-md break-words ${isChecked ? "line-through" : ""}`} 
+                className = {`flex-auto w-48 pt-1 text-md break-words ${isChecked ? "line-through" : ""}`} 
             >
                 {props.text}
             </p>
 
             <button 
-                className = "flex-auto py-2 font-bold transform transition duration-200 hover:opacity-80 hover:scale-150"
+                className = "flex-auto h-8 my-auto font-bold transform transition duration-200 hover:opacity-80 hover:scale-150"
                 onClick={() => {
                     props.delFunc(props.id)
                 }}
