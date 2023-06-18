56cbbe31857d5efc5ba119d8a666dac4723b8941
brebribre <70563428+brebribre@users.noreply.github.com>
2023-06-18 18:35:40 +0200

    tagline add

--- pages/components/App.tsx
+++ pages/components/App.tsx
@@ -5,8 +5,10 @@ import { useState, useEffect } from 'react';
 import Nav from './Nav'
 import Todos from './Todos'
 import Popup from './Popup'
+import Head from 'next/head'
 
 function App(){
+  
     const [bg, setBg] = useState("sakura");
     const [url, setUrl] = useState("bg-[url('/images/bg1.jpeg')]")
 
@@ -24,8 +26,15 @@ function App(){
     
     return (
     <div className={'bg-wrapper bg-no-repeat bg-center bg-fixed bg-cover ' + url}>
+      <Head>
+        <title>doMore.io | Aesthetic Podomoro Timer</title>
+        <link rel="shortcut icon" href="restart.svg" />
+      </Head>
       <div className = "bg-black h-screen w-full bg-opacity-10 flex flex-col items-center justify-center">
-
+        <div className = "absolute text-center top-5 sm:left-5 sm:text-left ">
+          <p className="text-3xl tracking-wide font-bold text-pink-800 italic">doMore.io</p>
+          <p className ="text-slate-300 font-semibold text-sm">by Bryan Alvin</p>
+        </div>
         <Timer />
         <Todos /> 
       </div>
