bbfbf13ffb0293f8f4e5ac236ed746a370349e2b
brebribre <70563428+brebribre@users.noreply.github.com>
2023-06-18 18:40:29 +0200

    size change

--- pages/components/Timer.tsx
+++ pages/components/Timer.tsx
@@ -85,7 +85,7 @@ export default function Timer(){
     
     return (
     <div>
-        <div className = "w-80 mb-8 gap-2 mx-auto grid grid-cols-3 text-center justify-center md:mb-4 text-sm md:text-md md:w-96 md:gap-4">
+        <div className = "w-80 mb-8 gap-2 mx-auto grid grid-cols-3 text-center justify-center md:mb-4 text-sm md:text-lg md:w-full md:gap-2">
             <div className = {`border-2 px-2 py-2 rounded-3xl font-semibold transform transition duration-200 hover:bg-white hover:text-black ${mode==='podomoro' ? "bg-white text-black bg-opacity-60 border-0" : "bg-transparent"}`} 
                 onClick = {() => {changeMode('podomoro')}}
             >
@@ -103,7 +103,7 @@ export default function Timer(){
             </div>
         </div>
 
-        <div className = "w-80 justify-center mx-auto text-7xl grid grid-cols-3 md:my-4 md:text-8xl md:w-80 ">
+        <div className = "w-80 justify-center mx-auto text-7xl grid grid-cols-3 md:my-4 md:text-9xl md:w-96 ">
             <div className ="text-slate-100 font-bold font-orbitron text-right">{oneDigit(minutes)? '0'+ minutes : minutes}</div>
             <div className ="text-slate-100 font-bold text-center">:</div>
             <div className ="text-slate-100 font-bold font-orbitron">{oneDigit(seconds)? '0'+seconds : seconds}</div>
