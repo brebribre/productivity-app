4f7899e94831db49761481c01347ff8ab690db7c
brebribre <70563428+brebribre@users.noreply.github.com>
2023-06-18 20:17:35 +0200

    asd

--- pages/components/Timer.tsx
+++ pages/components/Timer.tsx
@@ -85,8 +85,8 @@ export default function Timer(){
 
     
     return (
-    <div>
-        <div className = "w-80 mb-8 gap-2 mx-auto grid grid-cols-3 text-center justify-center md:mb-4 text-sm md:text-lg md:w-full md:gap-2">
+    <div className = "">
+        <div className = "w-80 mb-8 mt-12 gap-2 mx-auto grid grid-cols-3 text-center justify-center md:mb-4 text-sm md:text-lg md:w-full md:gap-2">
             <div className = {`border-2 px-2 py-2 rounded-3xl font-semibold transform transition duration-200 hover:bg-white hover:text-black ${mode==='podomoro' ? "bg-white text-black bg-opacity-60 border-0" : "bg-transparent"}`} 
                 onClick = {() => {changeMode('podomoro')}}
             >
