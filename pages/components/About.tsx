124f24c6f96ce491dda85b8b533b5b4718ef8bf2
brebribre <70563428+brebribre@users.noreply.github.com>
2023-06-22 03:08:43 +0200

    DONE!!!!

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -8,7 +8,7 @@ export default function About(){
             Have feature requests, or an issue to report? 
             Feel free to write me an email or contact me on Linkedin!
             </p>
-            <p className = "text-gray-400 text-sm mt-4">Don't forget to check out my other works in my <a className = "text-blue-400 underline" href="https://portfolio-brebribre.vercel.app/" target="_blank">personal website</a> also!</p>
+            <p className = "text-gray-400 text-sm mt-4">Don&apos;t forget to check out my other works in my <a className = "text-blue-400 underline" href="https://portfolio-brebribre.vercel.app/" target="_blank">personal website</a> also!</p>
             <p className ="mt-4 text-blue-300 text-sm"><span className = "mr-1">&#9993;</span> alvinbryan78@gmail.com</p>
             <div className = "flex mt-5">
                 <div className = "flex-initial mr-4">
